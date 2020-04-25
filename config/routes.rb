72a044809783e45d4a01724a7e4e4de4e5d17760
nour <nour7anelsto7y@gmail.com>
2020-04-25 21:55:31 +0200

    add signup and Signin features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles do
